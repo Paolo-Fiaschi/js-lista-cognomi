abc2dac4f89760780725461f1bfdddbf96cbd003
Paolo <p.fiaschi@hotmail.it>
2020-03-25 10:38:58 +0100

    lista non ordinata con slice

--- main.js
+++ main.js
@@ -13,6 +13,10 @@ var cognomeUtente, listaCognomi, cognomiNonOrdinati, cognomiOrdinati;
 // lista cognomi originaria
 listaCognomi = ["Messi", "Ronaldo", "Del Piero", "Baggio", "Zidane", "Dybala"];
 
+// lista cognomi copiata
+// listaCognomiCopy = listaCognomi.map(x => x);
+var listaCognomiCopy = listaCognomi.slice();
+
 console.log(listaCognomi);
 
 // chiedere all'utente il cognome
@@ -20,13 +24,16 @@ cognomeUtente = prompt("Inserisci il tuo cognome");
 
 console.log("il cognome aggiunto è: " + cognomeUtente);
 
-// aggiungere il cognome utente all'array
+// aggiungere il cognome utente all'array originale e copiato
 listaCognomi.push(cognomeUtente);
+
+listaCognomiCopy.push(cognomeUtente);
 
 console.log(listaCognomi);
 
 
 // ordinare la lista alfabeticamente
+listaCognomi.sort();
 
 console.log(listaCognomi);
 
@@ -34,12 +41,10 @@ document.getElementById('ordinata').innerHTML = "<h2 id='ordinata'>" + "Lista Or
 // ciclo per ogni item della listaCognomi ordinata e output
 for (var i = 0; i < listaCognomi.length; i++) {
 
-  listaCognomi.sort();
 
   cognomiOrdinati = document.getElementById('cognomiOrdinati').innerHTML;
 
   document.getElementById('cognomiOrdinati').innerHTML = cognomiOrdinati + "<li>" + listaCognomi[i] + "</li>";
-
 }
 console.log(cognomiOrdinati);
 
@@ -48,18 +53,14 @@ console.log(listaCognomi.indexOf(cognomeUtente) + 1);
 // posizione cognomeUtente
 document.getElementById('posizione').innerHTML = "<h3>" + "<span>" + cognomeUtente + "</span>" + " sei in posizione: " + (listaCognomi.indexOf(cognomeUtente) + 1) + "</h3>";
 
-
-
-
-
 document.getElementById('nonOrdinata').innerHTML = "<h2 id='nonOrdinata'>" + "Lista Non Ordinata" + "</h2>";
 
 // ciclo per ogni item della listaCognomi NON ordinata e output
-for (var i = 0; i < listaCognomi.length; i++) {
+for (var i = 0; i < listaCognomiCopy.length; i++) {
 
   cognomiNonOrdinati = document.getElementById('cognomiNonOrdinati').innerHTML;
 
-  document.getElementById('cognomiNonOrdinati').innerHTML = cognomiNonOrdinati + "<li>" + listaCognomi[i] + "</li>";
+  document.getElementById('cognomiNonOrdinati').innerHTML = cognomiNonOrdinati + "<li>" + listaCognomiCopy[i] + "</li>";
 
 }
 console.log(cognomiNonOrdinati);
